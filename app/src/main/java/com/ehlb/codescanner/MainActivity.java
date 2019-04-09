243d1d131cc34c15250051770a5fe4d07029de14
EmadHLB <emad.hlb@gmail.com>
2019-04-09 11:52:35 +0300

    Initial commit

---
+++ app/src/main/java/com/ehlb/codescanner/MainActivity.java
@@ -0,0 +1,58 @@
+package com.ehlb.codescanner;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ehlb.codescanner.Scanner.CodeScanner;
+import com.ehlb.codescanner.Scanner.CodeScannerView;
+import com.ehlb.codescanner.Scanner.DecodeCallback;
+import com.ehlb.com.codescanner.R;
+import com.google.zxing.Result;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
